a1fd68c393cfdde95fd86764fe99e77c5e909ec6
DAL3X <simon.bothe@gmx.de>
2023-06-29 14:20:59 +0200

    Added cartesian tree filling

--- CartesianGenerator.cpp
+++ CartesianGenerator.cpp
@@ -2,6 +2,26 @@
 #include "CartesianTree.h"
 
 
+/**
+* Fills the tree corresponding to this root with extra leaves as suggested in en.wikipedia.org/wiki/Range_minimum_query.
+* (Solution using constant time and linear space)
+* This is needed to make child on the left/right side of a nodes distinguishable.
+*/
+void fillCartesianTree(CartesianNode* node) {
+	if (node->getLeftChild() == nullptr) {
+		node->setLeftChild(new CartesianNode(ULLONG_MAX, node));
+	}
+	else {
+		fillCartesianTree(node->getLeftChild());
+	}
+	if (node->getRightChild() == nullptr) {
+		node->setRightChild(new CartesianNode(ULLONG_MAX, node));
+	}
+	else {
+		fillCartesianTree(node->getRightChild());
+	}
+}
+
 uint64_t CartesianGenerator::rangeMinimumQuery(std::vector<uint64_t> block, uint64_t min, uint64_t max) {
 	//return treeMap_->at(generateCartesianTree(block))->rangeMinimumQuery(min, max);
 	// TODO reactivate 
@@ -16,7 +36,9 @@ uint64_t CartesianGenerator::generateCartesianTree(std::vector<uint64_t> numbers
 			if (currentNode->getValue() <= numbers[i]) {
 				// Insert as right child and make previous right child the left child of the new node
 				CartesianNode* newNode = new CartesianNode(numbers[i], currentNode, currentNode->getRightChild(), nullptr);
-				currentNode->getRightChild()->setParent(newNode);
+				if (currentNode->getRightChild() != nullptr) {
+					currentNode->getRightChild()->setParent(newNode);
+				}
 				currentNode->setRightChild(newNode);
 				break;
 			}
@@ -30,7 +52,9 @@ uint64_t CartesianGenerator::generateCartesianTree(std::vector<uint64_t> numbers
 			currentNode = currentNode->getParent();
 		}
 	}
-	// TODO Fill tree up and transform it to uint64_t format
+	fillCartesianTree(tree->getRoot());
+	// TODO Transform it to uint64_t format
+	return 0;
 }
 
 void CartesianGenerator::generateAllCartesianTrees(uint64_t vectorSize) {
@@ -43,6 +67,11 @@ void CartesianGenerator::generateAllCartesianTrees(uint64_t vectorSize) {
 CartesianGenerator::CartesianGenerator(std::vector<std::vector<uint64_t>*>* blocks) {
 	treeMap_ = new std::unordered_map <uint64_t, NaiveRMQ*>();
 	generateAllCartesianTrees(blocks->at(0)->size()); // The first block is always completely filled and therefore tells us the cartesian tree size
+	// -----------------------------------------------------------------------------------
+	for (uint64_t i = 0; i < blocks->size(); i++) {
+		generateCartesianTree(*(blocks->at(i)));
+	}
+	// -----------------------------------------------------------------------------------
 	// TODO Generate NaiveRMQ for every block and insert them into treeMap_.
 }
 
